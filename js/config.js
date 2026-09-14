7ff504c91a9472edbfea9776eef7ef2f3847e8aa
Wallace Monsores <wallacemonsoress@gmail.com>
2026-09-13 21:24:52 -0300

    commit message: "Adição do projeto Catalog Leads"

--- js/config.js
+++ js/config.js
@@ -5,8 +5,8 @@
 const configuracaoPortfolio = {
   nome: "Wallace",
   area: "Desenvolvedor em formação",
-  foco: "Backend, APIs REST e aplicações web",
+  foco: "Backend, APIs REST",
   github: "https://github.com/wallace-d3v",
   linkedin: "https://www.linkedin.com/in/seu-perfil",
-  email: "wallace.back.developer@gmail.com"
+  email: "wallace.monsores.rj@gmail.com"
 };
